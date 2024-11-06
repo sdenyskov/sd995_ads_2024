29a84931b729303b4b4f545ab71d840cfd8e8f0f
Sviatoslav Denyskov <sd995@cam.ac.uk>
2024-11-06 15:05:44 +0000

    first commit after completing refresher

--- fynesse/access.py
+++ fynesse/access.py
@@ -11,6 +11,9 @@ import sqlite"""
 
 """Place commands in this file to access the data electronically. Don't remove any missing values, or deal with outliers. Make sure you have legalities correct, both intellectual property and personal data privacy rights. Beyond the legal side also think about the ethical issues around this data. """
 
+def hello_world():
+    print("Hello from the data science library!")
+
 def data():
     """Read the data from the web or local file, returning structured format such as a data frame"""
     raise NotImplementedError
