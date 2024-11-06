04271b27e2b063039609bb7fab32d262e36fc147
Sviatoslav Denyskov <sd995@cam.ac.uk>
2024-11-06 22:47:06 +0000

    final additions needed for lab 1

--- fynesse/access.py
+++ fynesse/access.py
@@ -1,6 +1,9 @@
 from .config import *
 
 import requests
+import pymysql
+import csv
+import time
 
 """These are the types of import we might expect in this file
 import httplib2
@@ -30,6 +33,50 @@ def download_price_paid_data(year_from, year_to):
             if response.status_code == 200:
                 with open("." + file_name.replace("<year>", str(year)).replace("<part>", str(part)), "wb") as file:
                     file.write(response.content)
+
+def create_connection(user, password, host, database, port=3306):
+    """ Create a database connection to the MariaDB database
+        specified by the host url and database name.
+    :param user: username
+    :param password: password
+    :param host: host url
+    :param database: database name
+    :param port: port number
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = pymysql.connect(user=user,
+                               passwd=password,
+                               host=host,
+                               port=port,
+                               local_infile=1,
+                               db=database
+                               )
+        print(f"Connection established!")
+    except Exception as e:
+        print(f"Error connecting to the MariaDB Server: {e}")
+    return conn
+
+def housing_upload_join_data(conn, year):
+    start_date = str(year) + "-01-01"
+    end_date = str(year) + "-12-31"
+
+    cur = conn.cursor()
+    print('Selecting data for year: ' + str(year))
+    cur.execute(f'SELECT pp.price, pp.date_of_transfer, po.postcode, pp.property_type, pp.new_build_flag, pp.tenure_type, pp.locality, pp.town_city, pp.district, pp.county, po.country, po.latitude, po.longitude FROM (SELECT price, date_of_transfer, postcode, property_type, new_build_flag, tenure_type, locality, town_city, district, county FROM pp_data WHERE date_of_transfer BETWEEN "' + start_date + '" AND "' + end_date + '") AS pp INNER JOIN postcode_data AS po ON pp.postcode = po.postcode')
+    rows = cur.fetchall()
+
+    csv_file_path = 'output_file.csv'
+
+    # Write the rows to the CSV file
+    with open(csv_file_path, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+    # Write the data rows
+    csv_writer.writerows(rows)
+    print('Storing data for year: ' + str(year))
+    cur.execute(f"LOAD DATA LOCAL INFILE '" + csv_file_path + "' INTO TABLE `prices_coordinates_data` FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED by '\"' LINES STARTING BY '' TERMINATED BY '\n';")
+    print('Data stored for year: ' + str(year))
 
 def data():
     """Read the data from the web or local file, returning structured format such as a data frame"""
