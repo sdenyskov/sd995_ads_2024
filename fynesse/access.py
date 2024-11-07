838f9440907e14f853d4d84cf97828fa6a15df4e
Sviatoslav Denyskov <sd995@cam.ac.uk>
2024-11-07 15:33:41 +0000

    added gitignire, sorted out the comments in access.py

--- fynesse/access.py
+++ fynesse/access.py
@@ -5,26 +5,38 @@ import pymysql
 import csv
 import time
 
-"""These are the types of import we might expect in this file
+"""
 import httplib2
 import oauth2
 import tables
 import mongodb
-import sqlite"""
+import sqlite
+"""
 
-# This file accesses the data
+"""
+This file is for accessing the data.
 
-"""Place commands in this file to access the data electronically. Don't remove any missing values, or deal with outliers. Make sure you have legalities correct, both intellectual property and personal data privacy rights. Beyond the legal side also think about the ethical issues around this data. """
+Place commands in this file to access the data electronically. 
+Don't remove any missing values, or deal with outliers. Make sure you have legalities correct, 
+both intellectual property and personal data privacy rights. 
+Beyond the legal side also think about the ethical issues around this data.
+"""
 
 def hello_world():
+
     print("Hello from the data science library!")
 
 def download_price_paid_data(year_from, year_to):
+    """
+    Download UK house price data for given year range
+    """
+
     # Base URL where the dataset is stored 
     base_url = "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com"
-    """Download UK house price data for given year range"""
+
     # File name with placeholders
     file_name = "/pp-<year>-part<part>.csv"
+
     for year in range(year_from, (year_to+1)):
         print (f"Downloading data for year: {year}")
         for part in range(1,3):
@@ -35,8 +47,9 @@ def download_price_paid_data(year_from, year_to):
                     file.write(response.content)
 
 def create_connection(user, password, host, database, port=3306):
-    """ Create a database connection to the MariaDB database
-        specified by the host url and database name.
+    """ 
+    Create a database connection to the MariaDB database 
+    specified by the host url and database name.
     :param user: username
     :param password: password
     :param host: host url
@@ -44,6 +57,7 @@ def create_connection(user, password, host, database, port=3306):
     :param port: port number
     :return: Connection object or None
     """
+    
     conn = None
     try:
         conn = pymysql.connect(user=user,
@@ -59,9 +73,11 @@ def create_connection(user, password, host, database, port=3306):
     return conn
 
 def housing_upload_join_data(conn, year):
+
     start_date = str(year) + "-01-01"
     end_date = str(year) + "-12-31"
 
+    # Select the data
     cur = conn.cursor()
     print('Selecting data for year: ' + str(year))
     cur.execute(f'SELECT pp.price, pp.date_of_transfer, po.postcode, pp.property_type, pp.new_build_flag, pp.tenure_type, pp.locality, pp.town_city, pp.district, pp.county, po.country, po.latitude, po.longitude FROM (SELECT price, date_of_transfer, postcode, property_type, new_build_flag, tenure_type, locality, town_city, district, county FROM pp_data WHERE date_of_transfer BETWEEN "' + start_date + '" AND "' + end_date + '") AS pp INNER JOIN postcode_data AS po ON pp.postcode = po.postcode')
@@ -72,14 +88,18 @@ def housing_upload_join_data(conn, year):
     # Write the rows to the CSV file
     with open(csv_file_path, 'w', newline='') as csvfile:
         csv_writer = csv.writer(csvfile)
-        # Write the data rows
         csv_writer.writerows(rows)
+    
     print('Storing data for year: ' + str(year))
     cur.execute(f"LOAD DATA LOCAL INFILE '" + csv_file_path + "' INTO TABLE `prices_coordinates_data` FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED by '\"' LINES STARTING BY '' TERMINATED BY '\n';")
     print('Data stored for year: ' + str(year))
+
     conn.commit()
 
 def data():
-    """Read the data from the web or local file, returning structured format such as a data frame"""
-    raise NotImplementedError
+    """
+    Read the data from the web or local file, 
+    returning structured format such as a data frame
+    """
 
+    raise NotImplementedError
